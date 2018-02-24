85bd8d35542ec738cc029573f48a4c799d8378b9
Angelo Luppino <afl1211@gmail.com>
2018-02-24 15:48:41 -0500

    Break out into templates

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone</strong>
+                    808.529.3819
+                </div>
+                <div class="column three">
+                    <strong>Location</strong>
+                    123 Kapiolani Boulevard<br>
+                    Honolulu, HI
+                </div>
+                <div class="column three last">
+                    <strong>Hours</strong>
+                    <em>Tuesday - Thursday</em><br>
+                    1:00pm - 9:00pm<br><br>
+                    <em>Friday - Saturday</em><br>
+                    4:00pm - 11:00pm<br><br>
+                    <em>Sinday - Monday</em><br>
+                    Closed<br><br>
+                </div>
+            </div>
+            <small>&copy; 2018 Angelo Luppino</small>
+        </div>
+    </div>
+
+</body>
+
+</html>
